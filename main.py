53c043020a1227110b954628b74ec180a40e4973
Kellogg Brengel <kellogg@brengel.com>
2025-04-10 17:46:39 -0500

    created main.py to read frankenstein and print

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    return print(get_book_text('./books/frankenstein.txt'))
+main()
