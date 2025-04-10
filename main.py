5be11c238f5d5ec637c861ebd489ae87658133bf
Kellogg Brengel <kellogg@brengel.com>
2025-04-10 18:08:16 -0500

    added function to count words in a strong

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def get_book_text(path_to_file):
+def count_words(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-    return file_contents
+    words = file_contents.split()
+    word_count = len(words)
+    return word_count
+    
 def main():
-    return print(get_book_text('./books/frankenstein.txt'))
+    return print(f"{count_words('./books/frankenstein.txt')} words found in the document")
 main()
